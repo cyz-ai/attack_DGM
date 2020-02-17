fcc3ef1bd4bd607c60e6f3ecbae19ef8502ada68
cyz-ai <32580308+cyz-ai@users.noreply.github.com>
2020-02-17 15:38:33 +0800

    first commit
    first commit

---
+++ datasets/__init__.py
@@ -0,0 +1,7 @@
+
+from .MNIST import load_mnist, load_mnist_pairs
+from .CIFAR import load_cifar10, load_cifar100
+from .SVHN import load_svhn, load_svhn_pairs
+from .FashionMNIST import load_fmnist, load_fmnist_pairs
+from .CelebA import load_celeba
+from .COIL import load_coil
